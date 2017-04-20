da0feefc5be31640fa7f19c87eee263ac75cfccb
k3v1n12 <ambalikevin@gmail.com>
2017-04-20 15:11:18 +0530

    Update exp5calculator.sh

--- exp5calculator.sh
+++ exp5calculator.sh
@@ -1,50 +1,34 @@
-#!/bin/sh
-#'Experiment 5'
-#'---------------'
-y=0
-while [ "$y" -eq 0 ]
+clear
+sum=0
+i="y"
+
+echo " Enter one no."
+read n1
+echo "Enter second no."
+read n2
+while [ $i = "y" ]
 do
-	echo "Enter Operand 1"
-	read a
-	echo "Enter Operand 2"
-	read b
-	x=0
-	while [ "$x" -eq 0 ]
-	do
-		echo "1.+"
-		echo "2.-"
-		echo "3.*"
-		echo "4./"
-		echo "5.%"
-		read e
-		if [ "$e" -eq 1 ]
-		then
-			c=$(($a+$b))
-			echo "Sum: $c"
-		elif [ "$e" -eq 2 ] 
-		then
-			c=$(($a-$b))
-			echo "Difference: $c" 
-		elif [ "$e" -eq 3 ]
-		then
-			c=$(($a\*$b)
-			echo "Product: $c"
-		elif [ "$e" -eq 4 ]
-		then
-			c=$(($a/$b))
-			echo "Quotient: $c"
-		elif [ "$e" -eq 5 ]
-		then
-			c=$(($a%$b))
-			echo "Remainder: $c"
-		else
-			echo "Invalid operation selected"
-		fi
-		echo "Press 0 to continue"
-		echo "Press 1 to exit"
-		read x
-	done
-	echo "Press 0 to continue"
-	echo "Press 1 to exit"
-	read y
-done
+echo "1.Addition"
+echo "2.Subtraction"
+echo "3.Multiplication"
+echo "4.Division"
+echo "Enter your choice"
+read ch
+case $ch in
+    1)sum=`expr $n1 + $n2`
+     echo "Sum ="$sum;;
+        2)sum=`expr $n1 - $n2`
+     echo "Sub = "$sum;;
+    3)sum=`expr $n1 \* $n2`
+     echo "Mul = "$sum;;
+    4)sum=`expr $n1 / $n2`
+     echo "Div = "$sum;;
+    *)echo "Invalid choice";;
+esac
+echo "Do u want to continue ?"
+read i
+if [ $i != "y" ]
+then
+    exit
+fi
+done 
